d56563c5c926d9e0154ac85a51f7ae236f14b121
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-05 16:32:39 +0900

    Userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
+  VALID_NAME_REGEX = /\A[a-z]+\z/
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: VALID_NAME_REGEX }
+  validates :profile, presence: true, length: { maximum: 200 }
 end
