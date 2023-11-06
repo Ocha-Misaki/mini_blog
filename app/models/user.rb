1fac249772a7fd280bccba30d5da09c6e2f4f906
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-06 13:12:23 +0900

    deviseのviewとcontrollerの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  VALID_NAME_REGEX = /\A[a-z]+\z/
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: VALID_NAME_REGEX }
+  validates :profile, presence: true, length: { maximum: 200 }
 end
