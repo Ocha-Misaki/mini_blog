5fb5f8f7272319f387382e2da693c08af258f0ef
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-06 16:57:53 +0900

    nameでログイン認証できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name profile email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name profile])
   end
 
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   VALID_NAME_REGEX = /\A[a-z]+\z/
   validates :name, presence: true, length: { maximum: 20 }, format: { with: VALID_NAME_REGEX }
   validates :profile, presence: true, length: { maximum: 200 }
