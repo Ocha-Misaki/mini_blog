4e4b21af1fc2f6ed34e0ca610c62d708608dfd34
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-11 14:38:17 +0900

    rspecにログイン時のテストを追加

--- spec/system/micropost_spec.rb
+++ spec/system/micropost_spec.rb
@@ -2,19 +2,23 @@ require 'rails_helper'
 describe '一覧表示機能' do
   before do
     user_a = FactoryBot.create(:user)
-    FactoryBot.create(:micropost, content: '最初のタスク', user: user_a)
+    FactoryBot.create(:micropost, content: 'テスト投稿', user: user_a)
   end
   context 'ユーザーAがログインしているとき' do
     before do
       # ユーザーAでログイン
       # ログイン画面に遷移
-      # メールアドレスを入力
+      visit new_user_session_path
+      # 名前を入力
+      fill_in 'Name', with: 'test'
       # パスワードを入力
+      fill_in 'パスワード', with: 'password'
       # ログインボタンを押す
+      click_button 'Log in'
     end
-    it 'ユーザーAが作成したタスクが表示される' do
+    it 'ユーザーAが作成した投稿が表示される' do
       visit root_path
-      expect(page).to have_content 'ミニブログへようこそ'
+      expect(page).to have_content 'テスト投稿'
     end
   end
 end
