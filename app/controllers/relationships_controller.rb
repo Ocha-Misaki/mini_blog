16255167536d7d3ebe4fa88a07dcb412901d3dc3
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-14 14:37:36 +0900

    フォロー機能削除の修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,12 +5,14 @@ class RelationshipsController < ApplicationController
       user = User.find(params[:followed_id])
       current_user.follow(user)
       redirect_to user
+    end
   end
 
   def destroy
     if user_signed_in?
-      user = Relationship.find(params[:id]).followed
+      user = current_user.active_relationships.find(params[:id]).followed
       current_user.unfollow(user)
       redirect_to user, notice: 'フォロー解除しました'
+    end
   end
 end
