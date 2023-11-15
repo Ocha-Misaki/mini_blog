433f7a324c3bc6f86df871035ca3a2d154d393ba
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-15 19:51:13 +0900

    before_actionにset_userを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[following followers]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -11,15 +12,19 @@ class UsersController < ApplicationController
 
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
+  end
+
+  private
+
+  def set_user
+    @user  = User.find(params[:id])
   end
 end
