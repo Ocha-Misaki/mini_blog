d02dee922827e1559c82502328e38a3673e14543
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-16 17:41:51 +0900

    ログイン関連のrspecテストを記述

--- spec/system/micropost_spec.rb
+++ spec/system/micropost_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
-Rspec.describe 'ユーザー' do
+RSpec.describe 'ユーザー' do
+  let(:user) { FactoryBot.create(:user, profile: 'テスト') }
   before do
-    let(user) { FactoryBot.create(:user, profile: 'そろそろ眠いです') }
   end
 
   context 'ログインしているとき' do
@@ -17,15 +17,16 @@ Rspec.describe 'ユーザー' do
 
     it 'プロフィールが閲覧できる' do
       visit user_profile_path
-      expect(page).to have_content 'そろそろ眠いです'
+      expect(page).to have_content 'テスト'
     end
 
     it '投稿の新規作成ができる' do
       visit new_user_micropost_path
-      fill_in '内容', with: 'おやすみなさい'
-      click_on '投稿する'
-      expect(page).to have_content '投稿されました'
+      fill_in 'Content', with: 'おやすみなさい'
+      click_on 'Post'
       expect(page).to have_content 'おやすみなさい'
     end
   end
+
+  
 end
