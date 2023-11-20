4f5bec41816939348f6f860ec3cca3e84b7146b3
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-20 15:10:48 +0900

    likesコントローラーの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,20 @@
 class Microposts::LikesController < ApplicationController
   before_action :authenticate_user!
   def create
+    ##　投稿詳細ページを作成する
+    @micropost = Micropost.find(params[:id])
+    if @micropost.user != current_user
+      @likes = Like.build(user_id: current_user.id,
+                          micropost_id: @micropost.id)
+      redirect_to user_path(@micropost.user), notice: 'いいねしました'
+    end
   end
 
   def destroy
+    @micropost = Micropost.find(params[:id])
+    @likes = Like.find_by(user_id: current_user.id,
+                          micropost_id: @micropost.id)
+    @likes.destroy!
+    redirect_to user_path(@micropost.user), notice: 'いいね削除しました'
   end
 end
