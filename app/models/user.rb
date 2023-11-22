d9477fe3ed4fb97b73d51722e212216b706e9b4a
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-11-22 12:12:35 +0900

    userモデルのfeedメソッドの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }, format: { with: VALID_NAME_REGEX }
   validates :profile, presence: true, length: { maximum: 200 }
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name: 'Relationship', inverse_of: :user, foreign_key: 'follower_id',
+  has_many :active_relationships, class_name: 'Relationship', inverse_of: :follower, foreign_key: 'follower_id',
                                   dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship', inverse_of: :user, foreign_key: 'followed_id',
+  has_many :passive_relationships, class_name: 'Relationship', inverse_of: :followed, foreign_key: 'followed_id',
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
@@ -17,13 +17,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   def feed
-    if following_ids.present?
-      Micropost.where("user_id IN (#{following_ids.join(',')})
-                      OR user_id = :user_id", user_id: id)
-               .includes(:user)
-    else
-      microposts
-    end
+    Micropost.where(user_id: following_ids << id).includes(:user).order(updated_at: :desc)
   end
 
   def follow(other_user)
@@ -37,5 +31,10 @@ class User < ApplicationRecord
   # 現在のユーザーが他のユーザーをフォローしていればtrueを返す
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def like?(micropost)
+    like = likes.find_by(micropost:)
+    like.presence
   end
 end
